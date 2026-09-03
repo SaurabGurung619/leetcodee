42d1bc2d6c345e84032dd2049b7f79c41fe0b854
Saurab Gurung <saurab20021121@gmail.com>
2026-09-03 23:09:39 +0530

    Time: 572 ms (74.11%) | Memory: 0B (100.00%) - LeetSync

---
+++ 262-trips-and-users/trips-and-users.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+select
+t.request_at as Day,
+round(avg(case 
+when t.status in ('cancelled_by_driver', 'cancelled_by_client')
+then 1
+else 0 
+end),2) as 'Cancellation Rate'
+from trips t
+join users c 
+on t.client_id = c.users_id
+
+join users d 
+on t.driver_id = d.users_id 
+
+where c.banned='No'
+and d.banned='No'
+and t.request_at between '2013-10-01' and '2013-10-03'
+group by t.request_at
